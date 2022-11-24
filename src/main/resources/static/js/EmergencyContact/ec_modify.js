dfd4b80f599b5b9de9a51790bc1827def99ed845
dclxxi <dclxxi@kakao.com>
2022-11-24 20:32:06 +0900

    feat: mapping emergency contact modify controller

---
+++ src/main/resources/static/js/EmergencyContact/ec_modify.js
@@ -0,0 +1,47 @@
+let saveStaff = {
+    init: function () {
+        $("#EC_Modify_button").on("click", () => {
+            this.save();
+        })
+    },
+    save: function () {
+        if ($("#name").val().length == 0) {
+            alert('이름을 입력해주세요.');
+        } else if ($("#corporation").val().length == 0) {
+            alert('소속회사를 입력해주세요.');
+        } else if ($("#group").val().length == 0) {
+            alert('조직을 입력해주세요.');
+        } else if ($("#position").val().length == 0) {
+            alert('직급을 입력해주세요.');
+        } else if ($("#phoneNumber").val().length == 0) {
+            alert('연락처를 입력해주세요.');
+        } else if ($("#email").val().length == 0) {
+            alert('이메일을 입력해주세요.');
+        }
+
+        let data = {
+            id: $("#id").val(),
+            name: $("#name").val(),
+            group: $("#group").val(),
+            email: $("#email").val(),
+            phoneNumber: $("#phoneNumber").val(),
+            corporation: $("#corporation").val(),
+            position: $("#position").val()
+        };
+
+        $.ajax({
+            method: "POST",
+            url: "/EmergencyContact/EC_Modify",
+            data: JSON.stringify(data),
+            contentType: "application/json"
+        }).done(function (resp) {
+            alert('비상연락망이 수정되었습니다.');
+            location.href = "/EmergencyContact/EC_Check"
+            console.log(JSON.stringify(data));
+        }).fail(function (xhr, status, error) {
+            console.log(error);
+        });
+    }
+}
+
+saveStaff.init();
